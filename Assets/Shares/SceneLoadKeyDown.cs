0b3990bd806b597b7a6731e92608f2b8c62d718e
dapin <89961651+dapin1490@users.noreply.github.com>
2022-12-05 13:45:04 +0900

    [edit] 씬 전환 스크립트 단축키 조합 변경, 콘솔 출력 추가
    - 단축키 조합을 컨트롤+숫자로 변경. 좌우 컨트롤, 키보드 숫자 키, 숫자패드 숫자 키 모두 가능
    - 씬 플레이시 실행되는 스크립트를 콘솔로 확인할 수 있도록 start()에 출력문 추가

--- Assets/Shares/SceneLoadKeyDown.cs
+++ Assets/Shares/SceneLoadKeyDown.cs
@@ -12,13 +12,19 @@ public class SceneLoadKeyDown : MonoBehaviour
 
     void Start()
     {
+        print("SceneLoadKeyDown script start"); // 콘솔 확인용 출력문 : 씬을 플레이할 때 어떤 스크립트가 실행되었는지 나타냄
         CurrentScene = gameObject.scene;
     }
 
     void Update()
     {
         // 현재 있는 씬으로 이동하는 키를 눌렀을 경우 씬 전환 안 되게 함
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        // 유니티 키 코드 참고 : https://docs.unity3d.com/kr/2021.3/ScriptReference/KeyCode.html
+        // 단축키 조합 : Ctrl + 1, 2, 3
+        // 키보드 위쪽 숫자 키, 숫자 패드 숫자 키 모두 가능
+        // 좌우 컨트롤 키 모두 가능
+        if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+            && (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)))
         {
             if (CurrentScene.name != UpperworldScene.name)
             {
@@ -26,7 +32,8 @@ public class SceneLoadKeyDown : MonoBehaviour
                 SceneManager.LoadScene(UpperworldScene.name);
             }
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+        if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+            && (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2)))
         {
             if (CurrentScene.name != MiddleworldScene.name)
             {
@@ -34,7 +41,8 @@ public class SceneLoadKeyDown : MonoBehaviour
                 SceneManager.LoadScene(MiddleworldScene.name);
             }
         }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
+        if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+            && (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3)))
         {
             if (CurrentScene.name != LowerworldScene.name)
             {
